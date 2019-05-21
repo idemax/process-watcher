068faf15f5d73b7300c9df612cf67cb9e8f367e3
Marcelo L. Filho <idemax@gmail.com>
2019-05-21 16:55:18 -0400

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# crash-counter
+
+## Install
+
+1. Node.js / NPM
+2. Clone the project
+3. Via terminal in the project folder execute `npm i`
+
+## Run
+
+Via terminal in the project folder execute: `node .\index.js "your_process_name"`
+
+Example: `node .\index.js "sublime_text.exe"`
+
+## Log
+
+In the project folder has a `logs` folder where will be saved every log JSON file.
---
+++ index.js
@@ -0,0 +1,82 @@
+const exec = require('child_process').execSync;
+const fs = require('fs-extra');
+const moment = require('moment');
+
+const startTime = moment();
+const logFolder = './logs';
+const procces2Watch = process.argv[2].trim();
+const logFileName = `${logFolder}/${moment().format("YYYY-MM-d")}.json`;
+
+var hasProcessRunning = false;
+
+if(!fs.existsSync(logFolder)) fs.mkdirSync(logFolder);
+
+function getLogJSON() {
+	if(!fs.existsSync(logFileName)) 
+		fs.writeFileSync(logFileName, JSON.stringify({}));
+
+	const logFile = JSON.parse(fs.readFileSync(logFileName).toString('utf8'));
+
+	if(!logFile.logs) logFile.logs = [];
+	
+	return logFile;
+}
+
+
+function registerUnityProcess(isClose=false) {
+	const logJSON = getLogJSON();
+	const countTerm = isClose ? 'closeCount' : 'openCount';
+	const countParam = logJSON[countTerm];
+	const count = !!countParam ? countParam : 0;
+
+	logJSON.logs.push({
+		process: procces2Watch,
+		status: isClose ? 'close' : 'open',
+		dateTime: moment().format("YYYY/MM/d HH:mm:SS")
+	});
+
+	logJSON[countTerm] = count + 1;
+
+	saveLogFile(logJSON);
+}
+
+function saveLogFile(json){
+	fs.writeFileSync(logFileName, JSON.stringify(json, null, 4));
+}
+
+function checkUnityProcess() {
+	const proccessRunning = exec('tasklist /fo csv')
+	.toString('utf8')
+	.trim()
+	.replace(/\"/gmi, '')
+	.split('\n')
+	.map(e => e.trim().split(','));
+
+	var processWasFound = false;
+	for (var i = 0; i < proccessRunning.length; i++) {
+		const processName = proccessRunning[i][0].toString().trim().toLowerCase();
+		
+		if(processName === procces2Watch) {
+			processWasFound = true;
+
+			if(!hasProcessRunning){
+				hasProcessRunning = true;
+				registerUnityProcess();
+			}
+
+			break;
+		}
+	}
+
+	if(!processWasFound){
+		if(hasProcessRunning){
+			hasProcessRunning = false;
+
+			registerUnityProcess(true);
+		}
+	}
+
+	setTimeout(checkUnityProcess,1000);
+}
+
+checkUnityProcess();
